c7b1abc5091d740b541c9b1eb265478086add3a4
Eric Lei <LeiEricSon@gmail.com>
2013-07-15 03:27:35 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,4 +1,17 @@
-var express = require('express');
+#!/usr/bin/env node
+   var express = require('express');
+   var app = express.createServer(express.logger());
+   var fs = require('fs');
+
+   app.get('/', function(request, response) {
+   //  response.send('Hello World2 !');
+   response.send(fs.readFileSync("index.html").toString()) ;
+   });
+
+   var port = process.env.PORT || 5000;
+   app.listen(port, function() {
+     console.log("Listening on " + port);
+   });var express = require('express');
 
 var app = express.createServer(express.logger());
 
