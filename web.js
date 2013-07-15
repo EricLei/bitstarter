9a2ee5615033e621861b4378e4294fb2f189f881
Eric Lei <LeiEricSon@gmail.com>
2013-07-15 03:31:44 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,22 +5,10 @@
 
    app.get('/', function(request, response) {
    //  response.send('Hello World2 !');
-   response.send(fs.readFileSync("index.html").toString()) ;
+   response.send(fs.readFileSync("index.html").toString());
    });
 
    var port = process.env.PORT || 5000;
    app.listen(port, function() {
      console.log("Listening on " + port);
-   });var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World from index.html!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
+   });
